d373bc551253017a47f84dea4a7187b3151bba71
andi smithers <andi.smithers@disney.com>
2014-08-05 20:27:47 -0700

    changed fonts. updated galaxymap

--- javascripts/MainGameBoard.js
+++ javascripts/MainGameBoard.js
@@ -127,7 +127,7 @@ BoardPiece.prototype.move = function(gameCycle)
 // help functoin to convert from map to screen space
 BoardPiece.prototype.screen = function(x, y)
 {
-  return {x:x*mapScale.x+mapScale.x*1.5+mapCentreX, y:y*mapScale.y+mapScale.y*1.5+mapCentreY};
+  return {x:x*mapScale.x+border.x*1.5+mapCentreX, y:y*mapScale.y+border.y*1.5+mapCentreY};
 }
 
 
